bdf17ed8ba46ec6cff24a9c69892b39a6c022885
Muh Ghazali Akbar <muhghazaliakbar@icloud.com>
2023-12-11 00:37:46 +0800

    Update header links hover styles

--- components/Header.tsx
+++ components/Header.tsx
@@ -15,24 +15,18 @@ const Header = () => {
             <div className="mr-3">
               <Logo />
             </div>
-            {typeof siteMetadata.headerTitle === 'string' ? (
-              <div className="hidden h-6 text-2xl font-semibold sm:block">
-                {siteMetadata.headerTitle}
-              </div>
-            ) : (
-              siteMetadata.headerTitle
-            )}
+            <h1 className="h-6 text-2xl font-semibold">{siteMetadata.headerTitle}</h1>
           </div>
         </Link>
       </div>
-      <div className="flex items-center space-x-4 leading-5 sm:space-x-6">
+      <div className="flex items-center gap-4 leading-5 sm:gap-6">
         {headerNavLinks
           .filter((link) => link.href !== '/')
           .map((link) => (
             <Link
               key={link.title}
               href={link.href}
-              className="hidden font-medium text-gray-900 dark:text-gray-100 sm:block"
+              className="hidden font-medium text-gray-900 transition-colors hover:text-primary-500 dark:text-gray-100 dark:hover:text-primary-500 sm:block"
             >
               {link.title}
             </Link>
